97c8a67342d311b54cb7fce6ff6ef765ed19ecb9
TheStriver97 <nileshh97@proton.me>
2024-02-19 17:20:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let element = document.getElementById('sketch-container')
+let isPressed = false;
+
+element.addEventListener('mousedown',()=>{isPressed=true})
+element.addEventListener('mouseup', ()=>{
+    isPressed=false
+})
+
+element.addEventListener('mousemove', (e)=>{
+    isPressed ? e.target.style.backgroundColor = 'red':e.target.backgroundColor='white'
+})
+
+
+for(let i = 0;i<12936;i++){element.appendChild(document.createElement('div'))}
